719ecd0013bee0d59b30d1a12d02b6691b2be70e
tiffe <tifferreira2005@gmail.com>
2026-06-02 19:42:00 -0300

    Estrutura inicial do projeto Spring Boot com H2 em modo file

---
+++ src/main/java/br/com/fiap/baselunar/BaseLunarApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.baselunar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseLunarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseLunarApplication.class, args);
+    }
+}
